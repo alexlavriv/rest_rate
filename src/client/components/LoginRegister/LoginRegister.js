d5cc8a482853c47c209e1651e6a7e1850f728ec1
Alex Lavriv <alex.lavriv@gmail.com>
2020-02-08 17:35:06 +0200

    added login and register modals

--- src/client/components/LoginRegister/LoginRegister.js
+++ src/client/components/LoginRegister/LoginRegister.js
@@ -1,16 +1,80 @@
 
- /* eslint-disable no-use-before-define */
 import React from 'react';
-
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+ /* eslint-disable no-use-before-define */
 import './LoginRegister.scss'
-export default function LoginRegister() {
-  return (
-    <div className="LoginResiter-root">
-      <div className="loginResiter-link">Login</div>
-      <div >|</div>
-      <div className="loginResiter-link">Register</div>
-    </div>
-       
-  );
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
 }
 
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+export default function SimpleModal() {
+  const classes = useStyles();
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+  const [hide, setHide] = React.useState(false);
+
+  const handleOpenRegister = () => {
+    console.log("handleOpenRegister");
+    setHide(true);
+    setOpen(true);
+  };
+  const handleOpenLogin = () => {
+    console.log("handleOpenLogin");
+    setHide(false);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+
+    setOpen(false);
+  };
+
+
+
+  return (
+    <div>
+      <div className="LoginResiter-root">
+      <div className="loginResiter-link" onClick={handleOpenLogin}>Login</div>
+      <div >|</div>
+      <div className="loginResiter-link" onClick={handleOpenRegister}>Register</div> </div>
+
+      <Modal
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        open={open}
+        onClose={handleClose}
+      >
+        <div style={modalStyle} className={classes.paper}>
+        <div style={hide===false?{'display':'block'}:{'display':'none'}}> Login</div>
+        <div style={hide===false?{'display':'none'}:{'display':'block'}}> Register</div>
+        <div></div>
+        </div>
+      </Modal>
+    </div>
+  );
+}
